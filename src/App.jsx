3ab5a6ea2ed83bbd845c449ac99aa2d141b2913f
facundozto <facundozerbato@outlook.com>
2025-04-08 14:39:58 -0300

    NavBar, Header, About, Services with Vite, React and Bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import NavBar from "./components/NavBar/NavBar";
+import Header from "./components/Header";
+import About from "./components/About/About";
+import Services from "./components/Services";
+/* import Clients from "./components/Clients";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer"; */
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Header />
+      <About />
+      <Services />
+      {/* <Clients />
+      <Contact />
+      <Footer />
+      <div className="text-center text-gray-500 text-xs">
+        <p>Developed by Estudio DH</p>
+      </div> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <header class="bg-dark py-5">
+      <div class="container px-5">
+        <div class="row gx-5 justify-content-center">
+          <div class="col-lg-6">
+            <div class="text-center my-5">
+              {/* <h1 class="display-5 fw-bolder text-white mb-2">D&H</h1> */}
+              <h2 class="display-6 fw-bolder text-white mb-2">ESTUDIO CONTABLE D&H</h2>
+              <p class="lead text-white-50 mb-4">Creamos una solución profesional a la medida de tus necesidades</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
